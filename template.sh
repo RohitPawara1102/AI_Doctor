a955b6a0e715d250335cc428129cb825483cb2f7
rohit <rohitpawara824@gmail.com>
2025-09-21 12:54:49 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+# creating files 
+tourch src/__init__.py
+tourch src/helper.py
+tourch src/prompt.py
+tourch.env
+tourch setup.py
+tourch app.py
+tourch research/trials.ipynb
+tourch requirements.txt
+
+
+echo "Directory and files created successfully."
